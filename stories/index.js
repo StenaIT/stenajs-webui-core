990e5bf62afcc76efd2659d8e485204069b19617
knegusen <knegusen@gmail.com>
2018-04-19 13:36:09 +0200

    Add support for typescript

--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,13 @@
+import {action} from '@storybook/addon-actions';
+import {withInfo} from '@storybook/addon-info';
+import {storiesOf} from '@storybook/react';
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import {Button} from '../src/components/Button';
 
 storiesOf('Button', module)
-    .add('with text', () => (
-        <Button onClick={action('clicked')}>Hello Button</Button>
-    ))
-    .add('with some emoji', () => (
-        <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-    ));
+    .add('with text',
+        withInfo({ inline: true })(() => <Button onClick={action('clicked')}>Hello Button</Button>)
+    )
+    .add('with some emoji',
+        withInfo({ inline: true })(() => <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>)
+    );
