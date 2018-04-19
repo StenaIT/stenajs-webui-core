12e7643b86bd530e194b759b93cc8baf4c6f92f0
knegusen <knegusen@gmail.com>
2018-04-19 13:39:52 +0200

    Add prettier

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export const Button = ({ children, onClick }) => (
-    <button onClick={onClick}>
-        { children }
-    </button>
+  <button onClick={onClick}>{children}</button>
 );
