0fcdb48c4d17bcc92f46b7614adc9db39f249dbc
Mattias Andersson <mattias800@gmail.com>
2018-09-10 13:21:28 +0200

    Create index.

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,14 +1,6 @@
 // created from 'create-ts-index'
 
-export * from './default-values/Colors';
+export * from './default-values';
+export * from './theme-types';
 export * from './DefaultTheme';
 export * from './Theme';
-export { ThemeFonts } from './theme-types/ThemeFonts';
-export { ThemeFontSizes } from './theme-types/ThemeFontSizes';
-export { ThemeColors } from './theme-types/ThemeColors';
-export { ThemeMetrics } from './theme-types/ThemeMetrics';
-export { ComponentThemes } from './theme-types/ComponentThemes';
-export { defaultFonts } from './default-values/DefaultFonts';
-export { defaultColors } from './default-values/DefaultColors';
-export { defaultFontSizes } from './default-values/DefaultFontSizes';
-export { defaultMetrics } from './default-values/DefaultMetrics';
