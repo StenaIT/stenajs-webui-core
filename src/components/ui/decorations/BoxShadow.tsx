19c27ad2dc48f3bea47aa04bf68681bb5d45f16e
knegusen <knegusen@gmail.com>
2018-09-14 13:58:28 +0200

    Add dummy component in BoxShadow

--- src/components/ui/decorations/BoxShadow.tsx
+++ src/components/ui/decorations/BoxShadow.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import styled from 'react-emotion';
 import { setDisplayName } from 'recompose';
 
@@ -6,3 +7,5 @@ export const BOX_SHADOW = 'rgba(0, 0, 0, 0.15) 0 2px 4px 0';
 export const BoxShadow = setDisplayName('BoxShadow')(styled('div')`
   box-shadow: ${BOX_SHADOW};
 `);
+
+export const __C_BOX_SHADOW = <div />;
