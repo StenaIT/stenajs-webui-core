66b478264f1235341d91b6525f2019875c89e754
knegusen <knegusen@gmail.com>
2018-09-18 08:57:35 +0200

    Add dummy component in Shadow

--- src/components/ui/decorations/Shadow.tsx
+++ src/components/ui/decorations/Shadow.tsx
@@ -1,6 +1,9 @@
+import * as React from 'react';
 import styled from 'react-emotion';
 import { setDisplayName } from 'recompose';
 
 export const Shadow = setDisplayName('Shadow')(styled('div')`
   box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 10px 4px;
 `);
+
+export const __SHADOW = <div />;
