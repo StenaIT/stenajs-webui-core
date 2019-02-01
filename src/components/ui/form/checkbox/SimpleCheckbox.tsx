18ae663e7360fd0188d122f73319fa77361de64e
Ludvig Lignell <ludvig.lignell@stenaline.com>
2019-02-01 13:29:12 +0100

    Checkbox possible to set different colors when component is checked or not

--- src/components/ui/form/checkbox/SimpleCheckbox.tsx
+++ src/components/ui/form/checkbox/SimpleCheckbox.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
+import styled from 'react-emotion';
 import { compose, pure, setDisplayName } from 'recompose';
 import { DeepPartial } from '../../../../types/DeepPartial';
-import {
-  withOnToggleHandler,
-  WithOnToggleHandler,
-} from '../../../util/enhancers/withOnToggleHandler';
 import {
   withComponentTheme,
   WithInnerComponentThemeProps,
 } from '../../../util/enhancers/WithComponentTheme';
-import { Background } from '../../colors/Background';
+import {
+  withOnToggleHandler,
+  WithOnToggleHandler,
+} from '../../../util/enhancers/withOnToggleHandler';
 import { Border } from '../../decorations/Border';
 import { Icon } from '../../icon/Icon';
 import { Clickable } from '../../interaction/Clickable';
@@ -26,6 +26,28 @@ type InnerProps = SimpleCheckboxProps &
   WithOnToggleHandler &
   WithInnerComponentThemeProps<SimpleCheckboxTheme>;
 
+const Wrapper = styled('div')<{
+  disabled: boolean;
+  theme: SimpleCheckboxTheme;
+  value: T;
+}>`
+  background-color: ${({ disabled, theme, value }) =>
+    disabled
+      ? theme.disabledColors.backgroundColor
+      : value
+        ? theme.checkedColors.backgroundColor
+        : theme.colors.backgroundColor};
+  border: 1px solid;
+  border-radius: ${({ theme }) => theme.borderRadius}px;
+  border-color: ${({ disabled, theme, value }) =>
+    disabled
+      ? theme.disabledColors.borderColor
+      : value
+        ? theme.checkedColors.borderColor
+        : theme.colors.borderColor};
+  overflow: hidden;
+`;
+
 export const SimpleCheckboxComponent = ({
   theme,
   disabled,
@@ -34,32 +56,26 @@ export const SimpleCheckboxComponent = ({
 }: InnerProps) => {
   return (
     <Clickable onClick={disabled ? undefined : onToggle}>
-      <Border
-        color={disabled ? theme.borderColorDisabled : theme.borderColor}
-        borderRadius={theme.borderRadius}
-        overflow={'hidden'}
-      >
-        <Background
-          color={
-            disabled ? theme.backgroundColorDisabled : theme.backgroundColor
-          }
+      <Wrapper disabled={disabled} theme={theme} value={value}>
+        <Row
+          justifyContent={'center'}
+          alignItems={'center'}
+          width={theme.width}
+          height={theme.height}
         >
-          <Row
-            justifyContent={'center'}
-            alignItems={'center'}
-            width={theme.width}
-            height={theme.height}
-          >
-            {value && (
-              <Icon
-                name={theme.checkIcon}
-                color={disabled ? theme.iconColorDisabled : theme.iconColor}
-                size={theme.iconSize}
-              />
-            )}
-          </Row>
-        </Background>
-      </Border>
+          {value && (
+            <Icon
+              name={theme.checkIcon}
+              color={
+                disabled
+                  ? theme.disabledColors.iconColor
+                  : theme.colors.iconColor
+              }
+              size={theme.iconSize}
+            />
+          )}
+        </Row>
+      </Wrapper>
     </Clickable>
   );
 };
--- src/components/ui/form/checkbox/SimpleCheckboxTheme.ts
+++ src/components/ui/form/checkbox/SimpleCheckboxTheme.ts
@@ -1,44 +1,65 @@
 import { IconProp } from '@fortawesome/fontawesome';
 import { defaultColors } from '../../../../themes/default-values/DefaultColors';
 
-export interface SimpleCheckboxTheme {
+export interface SimpleCheckboxStateTheme {
   borderColor: string;
-  borderColorDisabled: string;
   backgroundColor: string;
-  backgroundColorDisabled: string;
   iconColor: string;
-  iconColorDisabled: string;
+}
+
+export interface SimpleCheckboxTheme {
+  colors: SimpleCheckboxStateTheme;
+  disabledColors: SimpleCheckboxStateTheme;
+  checkedColors: SimpleCheckboxStateTheme;
   checkIcon: IconProp;
-  width: string;
-  height: string;
-  borderRadius: string;
+  width: number;
+  height: number;
+  borderRadius: number;
   iconSize: number;
 }
 
 export const defaultSimpleCheckboxTheme: SimpleCheckboxTheme = {
-  backgroundColor: defaultColors.white,
-  backgroundColorDisabled: '#f1f1f1',
-  borderColor: defaultColors.inputBorder,
-  borderColorDisabled: defaultColors.inputBorder,
-  iconColor: defaultColors.primaryText,
-  iconColorDisabled: defaultColors.disabledText,
+  colors: {
+    backgroundColor: defaultColors.white,
+    borderColor: defaultColors.inputBorder,
+    iconColor: defaultColors.primaryText,
+  },
+  disabledColors: {
+    backgroundColor: '#f1f1f1',
+    borderColor: defaultColors.inputBorder,
+    iconColor: defaultColors.disabledText,
+  },
+  checkedColors: {
+    backgroundColor: defaultColors.white,
+    borderColor: defaultColors.inputBorder,
+    iconColor: defaultColors.primaryText,
+  },
   checkIcon: 'check',
-  width: '22px',
-  height: '22px',
-  borderRadius: '4px',
+  width: 22,
+  height: 22,
+  borderRadius: 4,
   iconSize: 10,
 };
 
 export const defaultSimpleCheckboxThemeDark: SimpleCheckboxTheme = {
-  backgroundColor: '#b7d1d9',
-  backgroundColorDisabled: '#f1f1f1',
-  borderColor: 'transparent',
-  borderColorDisabled: defaultColors.inputBorder,
-  iconColor: '#4d7989',
-  iconColorDisabled: defaultColors.disabledText,
+  colors: {
+    backgroundColor: '#b7d1d9',
+    borderColor: 'transparent',
+    iconColor: '#4d7989',
+  },
+  disabledColors: {
+    backgroundColor: '#f1f1f1',
+    borderColor: defaultColors.inputBorder,
+    iconColor: defaultColors.disabledText,
+  },
+  checkedColors: {
+    backgroundColor: '#b7d1d9',
+    borderColor: defaultColors.inputBorder,
+    iconColor: defaultColors.primaryText,
+  },
   checkIcon: 'check',
-  width: '22px',
-  height: '22px',
-  borderRadius: '4px',
+  width: 22,
+  height: 22,
+  borderRadius: 4,
   iconSize: 10,
 };
