bae96588898c721b131bd625d7b17f007c43175f
Dennis Axelsson <knegusen@gmail.com>
2019-02-07 14:47:43 +0100

    Feature/more props to icon (#86)
    * Add more props to icon
    
    * Pass all props to FonteAwesomeIcon

--- src/components/ui/icon/Icon.tsx
+++ src/components/ui/icon/Icon.tsx
@@ -1,7 +1,13 @@
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import {
+  FlipProp,
+  IconProp,
+  RotateProp,
+  Transform,
+} from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
-import { compose, withProps } from 'recompose';
+import { CSSProperties } from 'react';
+import { compose, setDisplayName, withProps } from 'recompose';
 import { withTheme, WithThemeProps } from '../../util/enhancers';
 import { DefaultText } from '../text';
 
@@ -10,22 +16,48 @@ export type IconSource = 'fontawesome';
 export type IconProps = FontAwesomeProps;
 
 export interface FontAwesomeProps {
-  source?: IconSource;
-  name: IconProp;
-  size?: number;
+  className?: string;
   color?: string;
+  flip?: FlipProp;
+  name: IconProp;
+  pulse?: boolean;
+  rotation?: RotateProp;
+  size?: number;
+  source?: IconSource;
+  spin?: boolean;
+  style?: CSSProperties;
+  transform?: string | Transform;
 }
 
 type InnerProps = IconProps & WithThemeProps;
 
 const IconComponent = ({
-  name,
-  source = 'fontawesome',
-  size = 20,
+  className,
   color,
+  flip,
+  name,
+  pulse,
+  rotation,
+  size = 20,
+  source = 'fontawesome',
+  spin,
+  style,
+  transform,
 }: InnerProps) => {
   if (source === 'fontawesome') {
-    return <FontAwesomeIcon icon={name} style={{ fontSize: size, color }} />;
+    return (
+      <FontAwesomeIcon
+        className={className}
+        color={color}
+        flip={flip}
+        icon={name}
+        pulse={pulse}
+        rotation={rotation}
+        spin={spin}
+        style={{ fontSize: size, ...style }}
+        transform={transform}
+      />
+    );
   }
   return <DefaultText>Invalid icon source '{source}'</DefaultText>;
 };
@@ -37,7 +69,9 @@ const withDefaultColor = withProps<
   color: color || theme.colors.primaryTextLight,
 }));
 
-export const Icon = compose<InnerProps, IconProps>(
-  withTheme,
-  withDefaultColor,
-)(IconComponent);
+export const Icon = setDisplayName<IconProps>('Icon')(
+  compose<InnerProps, IconProps>(
+    withTheme,
+    withDefaultColor,
+  )(IconComponent),
+);
---
+++ stories/icon/IconStories.tsx
@@ -0,0 +1,84 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+  faCheckCircle as farCheckCircle,
+  faCircle as farCircle,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faBicycle,
+  faClock,
+  faCog,
+  faShip,
+  faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
+import { withInfo } from '@storybook/addon-info';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { Icon } from '../../src/components/ui/icon';
+import { Indent, Row, Space } from '../../src/components/ui/layout';
+import { DefaultText } from '../../src/components/ui/text';
+
+library.add(
+  farCheckCircle,
+  farCircle,
+  faBicycle,
+  faClock,
+  faCog,
+  faShip,
+  faSpinner,
+);
+
+export const addIconStories = () => {
+  storiesOf('Icon', module)
+    .addDecorator(withInfo())
+    .add('default', () => <Icon name={'cog'} />)
+    .add('with color', () => <Icon name={'cog'} color={'#abcdef'} />)
+    .add('with spin', () => <Icon name={'spinner'} spin={true} />)
+    .add('with pulse', () => <Icon name={'spinner'} pulse={true} />)
+    .add('horizontal flip', () => (
+      <>
+        <Row>
+          <DefaultText>Normal</DefaultText>
+          <Space />
+          <Icon name={'bicycle'} />
+        </Row>
+        <Row>
+          <DefaultText>Flipped</DefaultText> <Space />
+          <Icon name={'bicycle'} flip={'horizontal'} />
+        </Row>
+      </>
+    ))
+    .add('vertical flip', () => (
+      <>
+        <Row>
+          <DefaultText>Normal</DefaultText>
+          <Space />
+          <Icon name={'bicycle'} />
+        </Row>
+        <Row>
+          <DefaultText>Flipped</DefaultText> <Space />
+          <Icon name={'bicycle'} flip={'vertical'} />
+        </Row>
+      </>
+    ))
+    .add('with rotation', () => (
+      <Row>
+        <Indent>
+          <DefaultText>90</DefaultText>
+          <Space />
+          <Icon name={'ship'} rotation={90} />
+        </Indent>
+        <Indent>
+          <DefaultText>180</DefaultText> <Space />
+          <Icon name={'ship'} rotation={180} />
+        </Indent>
+        <Indent>
+          <DefaultText>270</DefaultText> <Space />
+          <Icon name={'ship'} rotation={270} />
+        </Indent>
+      </Row>
+    ))
+    .add('with transform', () => (
+      <Icon name={'bicycle'} transform={{ flipX: true, rotate: 90 }} />
+    ));
+};
