89f2214b813fce6dbe0d570aaac78ca4db5571f7
knegusen <knegusen@gmail.com>
2019-02-15 15:14:21 +0100

    Change legacy ref again

--- src/components/RequiredComponentProps.ts
+++ src/components/RequiredComponentProps.ts
@@ -1,11 +1,11 @@
 import { Ref } from 'react';
-import * as React from 'react';
 
 export interface RequiredComponentProps<T> {
   className?: string;
   ref?: Ref<T>;
 }
 
-export interface RequiredInputComponentProps<T> extends RequiredComponentProps<T> {
-  inputRef?: React.LegacyRef<T>;
+export interface RequiredInputComponentProps<T>
+  extends RequiredComponentProps<T> {
+  inputRef?: Ref<T>;
 }
