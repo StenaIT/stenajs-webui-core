51af5286dcc7e5df3fa8eb34c368bd1b60012063
knegusen <knegusen@gmail.com>
2019-02-15 14:55:04 +0100

    Make ref, classname and inputRef optional

--- src/components/RequiredComponentProps.ts
+++ src/components/RequiredComponentProps.ts
@@ -2,10 +2,10 @@ import { Ref } from 'react';
 import * as React from 'react';
 
 export interface RequiredComponentProps<T> {
-  className: string;
-  ref: Ref<T>;
+  className?: string;
+  ref?: Ref<T>;
 }
 
 export interface RequiredInputComponentProps<T> extends RequiredComponentProps<T> {
-  inputRef: React.LegacyRef<T>;
+  inputRef?: React.LegacyRef<T>;
 }
