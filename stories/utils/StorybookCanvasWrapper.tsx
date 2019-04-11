520d8045849fbf9d3dd92e098f1c2d6873e48742
Kalle Ericson <kalle.ericson@stenaline.com>
2019-04-11 11:11:11 +0200

    Less height in StorybookCanvas

--- stories/utils/StorybookCanvasWrapper.tsx
+++ stories/utils/StorybookCanvasWrapper.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 
 interface Props {
-  children: any
+  children: React.ReactNode;
 }
 
-export const StorybookCanvasWrapper: React.FC<Props> = ({
-  children
-}) => (
-    <div style={{ height: '100vh', width: '100%', display: 'flex', 'alignItems': 'center', justifyContent: 'center' }}>
-      <div style={{ display: 'inline-block' }}>
-        <div style={{ display: 'table' }}>
-          {children}
-        </div>
-      </div>
+export const StorybookCanvasWrapper: React.FC<Props> = ({ children }) => (
+  <div
+    style={{
+      height: '50vh',
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <div style={{ display: 'inline-block' }}>
+      <div style={{ display: 'table' }}>{children}</div>
     </div>
-  );
+  </div>
+);
